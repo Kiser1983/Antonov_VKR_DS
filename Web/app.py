4d9f8b8f8195059055facabf906a69d0564bfcb2
Sergey Antonov <Antonov-s@mail.ru>
2022-04-09 05:25:36 +0400

    Update web

--- Web/app.py
+++ Web/app.py
@@ -1,27 +1,18 @@
-import joblib
+from tensorflow import keras
 import numpy as np
 import pandas as pd
-from flask import Flask, request, render_template
-from model import model
 
+from flask import Flask, request, render_template
+from keras.models import load_model
+import pandas as pd
 
 app = Flask(__name__)
 
 
-def get_prediction(X_test):
-    prediction = model.predict(X_test)
-    return str(prediction)
-
-
-#@app.route('/')
-#def index():
-
- #   return model
-
-
 @app.route('/predict/', methods=['post', 'get'])
-def index():
-    message = ''
+def compute():
+    result = ''
+    error = dict()
     if request.method == 'POST':
         var1 = request.form.get('Плотность, кг/м3')
         var2 = request.form.get('модуль упругости, ГПа')
@@ -35,21 +26,12 @@ def index():
         var10 = request.form.get('Угол нашивки, град')
         var11 = request.form.get('Шаг нашивки')
         var12 = request.form.get('Плотность нашивки')
-        X_test = pd.DataFrame([[var1, var2, var3, var4,
-                           var5, var6, 0.0, 0.0, var9, var10, var11,var12]])
-
-        scaler = trans.transform(X_test)
-        standart = df_standart.transform(scaler)
-        PCA = pca.transform(standart)
-        df_pca_new= pca.inverse_transform(PCA)
-        df_pca = pd.DataFrame(df_pca_new, columns=X_test.columns)
-
-        message = 'Соотношение матрица-наполнитель:', get_prediction(df_pca)
-
-    return render_template('login.html', message=message)
+        input = pd.DataFrame([[var1, var2, var3, var4, var5, var6, 0.0, 0.0, var9, var10, var11,var12]])
+        result = model.predict(input)[0][0]
+    return render_template('index.html', result=result, error=error)
 
 
 if __name__ == '__main__':
-    model = keras.models.load_model('Models/model11.h5')
+    model = load_model('Models/')
+    app.run(host='localhost', port=8081, debug=True)
 
-    app.run()
